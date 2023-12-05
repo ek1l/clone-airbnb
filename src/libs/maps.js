8769c765d5fd507347143196e08ae7de19c0c3ec
Leonardo Duarte <hintl3rgames2015@gmail.com>
2023-12-05 00:05:25 -0300

    Adicionando mapas alternados

--- src/libs/maps.js
+++ src/libs/maps.js
@@ -1,33 +1,60 @@
 const L = window.L || {};
 
-const defaultOptions = { zoom: 13, coords: [], maxZoom: 18 };
-
-export const initialize = ($element, options = defaultOptions) => {
-  const map = L.map($element).setView(options.coords, options.zoom);
-  L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution:
-      '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>',
-    titleSize: 512,
-  }).addTo(map);
-
-  const markers = L.featureGroup()
-    .on('mouseover', (e) => {
-      e.layer.setZIndexOffset(100);
-    })
-    .on('mouseout', (e) => {
-      e.layer.setZIndexOffset(0);
-    })
-    .addTo(map);
-};
-
 const inconPrice = (content) => {
   return L.divIcon({ className: 'map-price', html: content, iconSize: null });
 };
 
-// const addMarker = (attrs = {}) => {
-//   mapInstance.markers.addLayer(
-//     L.marker(attrs.coords, {
-//       icon: inconPrice(attrs.content),
-//     }),
-//   );
-// };
+const defaultOptions = {
+  zoom: 13,
+  coords: [],
+  maxZoom: 18,
+  tileServer: 'maptiler',
+};
+
+const mapTileConfig = {
+  mapDefault: {
+    url: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+    options: {
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+      titleSize: 512,
+    },
+  },
+  maptiler: {
+    url: 'https://api.maptiler.com/maps/{id}/{z}/{x}/{y}.png?key={key}',
+    options: {
+      key: '3U5wYD1rEGBHqHyEGhcb',
+      id: 'basic',
+    },
+  },
+};
+
+const initialize = ($element, options = defaultOptions) => {
+  const optionsBase = {
+    ...defaultOptions,
+    ...options,
+  };
+
+  const map = L.map($element).setView(optionsBase.coords, optionsBase.zoom);
+  L.tileLayer(
+    mapTileConfig[optionsBase.tileServer].url,
+    mapTileConfig[optionsBase.tileServer].options,
+  ).addTo(map);
+
+  const markers = L.featureGroup().addTo(map);
+
+  const addMarker = (attrs = {}) => {
+    markers.addLayer(
+      L.marker(attrs.coords, {
+        riseOnHover: true,
+        icon: inconPrice(attrs.content),
+      }),
+    );
+  };
+
+  return {
+    addMarker,
+  };
+};
+
+export default initialize;
