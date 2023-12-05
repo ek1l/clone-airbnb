608301bd5700f60c6db4bf8e2a5f5450a26f91c0
Leonardo Duarte <hintl3rgames2015@gmail.com>
2023-12-04 23:24:38 -0300

    Primeiras refatorações

--- src/libs/maps.js
+++ src/libs/maps.js
@@ -1,47 +1,33 @@
 const L = window.L || {};
 
-const mapInstance = {
-  map: null,
-  layer: null,
-  markers: L.featureGroup(),
-};
-
 const defaultOptions = { zoom: 13, coords: [], maxZoom: 18 };
 
-function initialize($element, options = defaultOptions) {
-  mapInstance.map = L.map($element).setView(options.coords, options.zoom);
-  mapInstance.layer = L.tileLayer(
-    'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
-    {
-      attribution:
-        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>',
-      titleSize: 512,
-    },
-  ).addTo(mapInstance.map);
+export const initialize = ($element, options = defaultOptions) => {
+  const map = L.map($element).setView(options.coords, options.zoom);
+  L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution:
+      '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+    titleSize: 512,
+  }).addTo(map);
 
-  mapInstance.markers
+  const markers = L.featureGroup()
     .on('mouseover', (e) => {
       e.layer.setZIndexOffset(100);
     })
     .on('mouseout', (e) => {
       e.layer.setZIndexOffset(0);
     })
-    .addTo(mapInstance.map);
-}
-
-function inconPrice(content) {
-  return L.divIcon({ className: 'map-price', html: content, iconSize: null });
-}
-
-function addMarker(attrs = {}) {
-  mapInstance.markers.addLayer(
-    L.marker(attrs.coords, {
-      icon: inconPrice(attrs.content),
-    }),
-  );
-}
-
-export default {
-  initialize,
-  addMarker,
+    .addTo(map);
 };
+
+const inconPrice = (content) => {
+  return L.divIcon({ className: 'map-price', html: content, iconSize: null });
+};
+
+// const addMarker = (attrs = {}) => {
+//   mapInstance.markers.addLayer(
+//     L.marker(attrs.coords, {
+//       icon: inconPrice(attrs.content),
+//     }),
+//   );
+// };
--- src/main.js
+++ src/main.js
@@ -1,38 +1,38 @@
-import maps from './libs/maps.js';
+import { initialize } from './libs/maps.js';
 import { getPosition } from './helpers/geolocation.js';
 
 async function init() {
   const { coords } = await getPosition();
 
-  maps.initialize(document.querySelector('.map-instance'), {
+  initialize(document.querySelector('.map-instance'), {
     coords: [coords.latitude, coords.longitude],
     zoom: 13,
   });
 
-  maps.addMarker({
-    coords: [-19.45442371086181, -44.25036484452751],
-    content: 'R$ 400',
-  });
+  // maps.addMarker({
+  //   coords: [-19.45442371086181, -44.25036484452751],
+  //   content: 'R$ 400',
+  // });
 
-  maps.addMarker({
-    coords: [-19.467534923063994, -44.25310914915545],
-    content: 'R$ 350',
-  });
+  // maps.addMarker({
+  //   coords: [-19.467534923063994, -44.25310914915545],
+  //   content: 'R$ 350',
+  // });
 
-  maps.addMarker({
-    coords: [-19.47594809554594, -44.23370628079845],
-    content: 'R$ 5475',
-  });
+  // maps.addMarker({
+  //   coords: [-19.47594809554594, -44.23370628079845],
+  //   content: 'R$ 5475',
+  // });
 
-  maps.addMarker({
-    coords: [-19.436779930397318, -44.246248500167404],
-    content: 'R$ 1600',
-  });
+  // maps.addMarker({
+  //   coords: [-19.436779930397318, -44.246248500167404],
+  //   content: 'R$ 1600',
+  // });
 
-  maps.addMarker({
-    coords: [-19.468180651831954, -44.24315148247979],
-    content: 'R$ 2000',
-  });
+  // maps.addMarker({
+  //   coords: [-19.468180651831954, -44.24315148247979],
+  //   content: 'R$ 2000',
+  // });
 }
 
 export default init;
